e2037af2f60b38859c2b78d50ddac026cb6ad394
Jelly <Jellybob2016@outlook.com>
2021-08-30 07:17:27 +0100

    Test

---
+++ src/main/java/dev/elliotfrost/anarchybot/command/CommandContext.java
@@ -0,0 +1,24 @@
+package dev.elliotfrost.anarchybot.command;
+
+import me.duncte123.botcommons.commands.ICommandContext;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.List;
+
+public record CommandContext(GuildMessageReceivedEvent event, List<String> args) implements ICommandContext {
+
+    @Override
+    public Guild getGuild() {
+        return this.getEvent().getGuild();
+    }
+
+    @Override
+    public GuildMessageReceivedEvent getEvent() {
+        return this.event;
+    }
+
+    public List<String> getArgs() {
+        return this.args;
+    }
+}
