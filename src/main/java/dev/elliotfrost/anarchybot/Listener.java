b4a9461a869eadfc92d1d49151e50a1672627883
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-09-09 20:08:25 -0500

    Beginning of `talk-to-the-admins` v2, needs finetuning

--- src/main/java/dev/elliotfrost/anarchybot/Listener.java
+++ src/main/java/dev/elliotfrost/anarchybot/Listener.java
@@ -1,6 +1,9 @@
 package dev.elliotfrost.anarchybot;
 
 import me.duncte123.botcommons.BotCommons;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
@@ -13,6 +16,9 @@ import net.dv8tion.jda.api.requests.restaction.pagination.ReactionPaginationActi
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Listener extends ListenerAdapter {
@@ -107,48 +113,76 @@ public class Listener extends ListenerAdapter {
 
     @Override
     public void onSelectionMenu(SelectionMenuEvent event) {
-        if (event.getComponentId().equals("ci is gay ")) {
-            int catId;
+        if (event.getComponentId().equals("menu:tickets")) {
+            String user = event.getUser().getName() + "#" + event.getUser().getDiscriminator(), ticketnum = "dbpull go here";
+            event.reply("Making ticket :)")
+                    .setEphemeral(true)
+                    .queue();
+            event.getChannel().sendTyping().queue();
+            String title = "*** ***", descr = "*** ***", java = "*** ***", br = "*** ***", icon = "*** ***";
+            Color color = Color.BLUE;
+            // It should be here, so that the user is immediately notified instead of waiting for other code to run.
             switch (event.getValues().get(0)) {
                 case "bug":
-                    catId = 1;
+                    color = Color.RED;
+                    title = "Bug Report";
+                    descr = "A bug report";
+                    java = "testjavausername";
+                    br = "testbrusername";
+                    icon = "https://www.skynode.pro/favicon-192x192.png";
                     break;
 
                 case "report":
-                    catId = 1;
+                    color = Color.lightGray;
+                    title = "Player Report";
+                    descr = "A Player report";
+                    java = "testjavausername";
+                    br = "testbrusername";
+                    icon = "https://www.skynode.pro/favicon-192x192.png";
                     break;
 
                 case "support":
-                    catId = 1;
+                    color = Color.GREEN;
+                    title = "Support";
+                    descr = "General Support";
+                    java = "testjavausername";
+                    br = "testbrusername";
+                    icon = "https://www.skynode.pro/favicon-192x192.png";
                     break;
 
                 default:
-                    catId = 1;
+                    color = Color.GREEN;
+                    title = "Other";
+                    descr = "Other things you may need to talk to the admin for";
+                    java = "testjavausername";
+                    br = "testbrusername";
+                    icon = "https://www.skynode.pro/favicon-192x192.png";
+                    break;
                     // other
-                }
-                String user = event.getUser().getName() + "#" + event.getUser().getDiscriminator(), ticketnum = "dbpull go here";
-                event.reply("Making ticket :)")
-                        .setEphemeral(true)
-                        .queue();
-                event.getChannel().sendTyping().queue();
+            }
 
-                // createTextChannel(String name, Category parent) - Everything else uses .<actions>
-                // "%s's support ticket || has had %s ticket(s)"
-                Objects.requireNonNull(event.getGuild()).createTextChannel(user,  event.getGuild().getCategoryById(catId))
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+
+            Objects.requireNonNull(event.getGuild()).createTextChannel(user + " - " + ticketnum,  event.getGuild().getCategoryById("867535554253684807"))
                         .addMemberPermissionOverride(event.getUser().getIdLong(), 1024, 0)
                         .addRolePermissionOverride(Objects.requireNonNull(event.getGuild().getRoleById("866757654244622366")).getIdLong(), 0 ,3072)
-                        .queue();
-                // DB Code for adding a ticket to user's total here
+                        .setTopic(user + "'s support ticket || has had " + ticketnum + " ticket(s)")
+                        .queue(); // This needs to be a variable, so that we can send a message in this channel.
+            // DB Code for adding a ticket to user's total here
                 // DB code for getting a user's info (linked accts etc)
-                // IT WONT LET ME PASTE
-                // Embed Code ^
+
+            MessageEmbed embed = new EmbedBuilder()
+                    .setTitle(title)
+                    .setDescription(descr)
+                    .setColor(color)
+                    .addField("Java Minecraft User:", java, false)
+                    .addField("Bedrock Minecraft User:", br, false)
+                    .addField("Ticket Created (GMT):", dtf.format(now), false)
+                    .setImage(icon)
+                    .setFooter("Ticket Made for" + user)
+                    .build();
+            event.getChannel().sendMessageEmbeds(embed).queue();
         }
     }
-
-
-    // Also, just a suggestion. Instead of generating each embed inside a switch statement.
-    // I advise you just set the string in each, and then build the embed with the string variable at the end
-
-    // smart!
-    // big brain
 }
