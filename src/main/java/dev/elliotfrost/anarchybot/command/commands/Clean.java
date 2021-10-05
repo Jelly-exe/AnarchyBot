4ce70d4d8ca79ea09bccd8a3a31503c6adf2836b
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-10-04 21:45:51 -0500

    Add error Catcher for Clean

--- src/main/java/dev/elliotfrost/anarchybot/command/commands/Clean.java
+++ src/main/java/dev/elliotfrost/anarchybot/command/commands/Clean.java
@@ -3,18 +3,28 @@ package dev.elliotfrost.anarchybot.command.commands;
 import dev.elliotfrost.anarchybot.command.CommandContext;
 import dev.elliotfrost.anarchybot.command.ICommand;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageHistory;
-import net.dv8tion.jda.api.requests.restaction.pagination.MessagePaginationAction;
 
-import java.util.Collection;
 import java.util.List;
 
 public class Clean implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
         List<String> args = ctx.getArgs();
-        List<Message> messages = ctx.getChannel().getHistory().retrievePast(Integer.parseInt(args.get(0))).complete();
-        ctx.getChannel().deleteMessages(messages).complete();
+        try {
+            List<Message> messages = ctx.getChannel()
+                    .getHistory()
+                    .retrievePast(Integer.parseInt(args.get(0)))
+                    .complete();
+            ctx.getChannel()
+                    .deleteMessages(messages)
+                    .complete();
+        } catch (Exception e) {
+            ctx.getChannel()
+                    .sendMessage("Error while running command:\n`" + e.getMessage() + "`\n|| The most common error is specifying a value less than 2 or greater than 100. ||")
+                    .queue(message ->
+                            message.addReaction("U+1F62D")
+                                    .queue());
+        }
     }
 
     @Override
