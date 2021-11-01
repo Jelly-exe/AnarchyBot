e13a208e2daef6bfab61b64584f74f7824f2d640
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-11-01 11:45:30 -0500

    Fixes and improvements to Cmd
    Add error detection
    
    Make BANNED_COMMANDS and env Variable
    
    Clean no perms error

--- src/main/java/dev/elliotfrost/anarchybot/command/commands/Cmd.java
+++ src/main/java/dev/elliotfrost/anarchybot/command/commands/Cmd.java
@@ -7,26 +7,35 @@ import dev.elliotfrost.anarchybot.command.CommandContext;
 import dev.elliotfrost.anarchybot.command.ICommand;
 import dev.elliotfrost.anarchybot.functions.CheckArrayContains;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Cmd implements ICommand {
+    private String error;
+
     @Override
     public void handle(CommandContext ctx) {
         JDA jda = ctx.getJDA();
         List<String> args = ctx.getArgs();
-        List<String> nono = Arrays.asList(Config.get("BANNED-COMMANDS").split(","));
-        String command = String.join(" ", args); // Join the arguments into one string
+        List<String> nono = Arrays.asList(Config.get("BANNED_COMMANDS").split(","));
+        String command = ctx.getArgs().toString();
         boolean hasbadcommand = new CheckArrayContains().Checker(args, nono);
+        if (ctx.getMember().hasPermission(Permission.ADMINISTRATOR)) hasbadcommand = false;
         if (hasbadcommand) {
-            ctx.getChannel().sendMessage("You cannot perform command: " + command + "\nReason: insufficient permissions").queue();
+            ctx.getChannel().sendMessage("You cannot perform command: `" + command + "` because you do not have the permission.").queue();
         } else {
             PteroClient api = PteroBuilder.createClient("https://panel.skynode.pro", Config.get("PTERO_TOKEN"));
             api.retrieveServerByIdentifier(Config.get("ANARCHY-SERVER-ID"))
-                    .flatMap(server -> server.sendCommand(command))
+                    .flatMap(server -> server.sendCommand(command)
+                            .onErrorMap(throwable -> { this.error = throwable.getMessage();
+                        return null;
+                    }))
                     .execute();
-            ctx.getChannel().sendMessage("Command Executed").queue();
+            StringBuilder message = new StringBuilder().append("Command executed!");
+            if (this.error != null) message.append("\nError returned: ").append(this.error);
+            ctx.getChannel().sendMessage(message).queue();
         }
     }
 
@@ -37,7 +46,7 @@ public class Cmd implements ICommand {
 
     @Override
     public List<String> getAliases() {
-        return List.of();
+        return List.of("execute", "exec");
     }
 }
 
