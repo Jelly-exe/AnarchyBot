bb8dc8bcaac30770f9a966d8c0ecc6daa0904c9e
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-11-02 13:13:59 -0500

    checkmessage method in NewStatuses

--- src/main/java/dev/elliotfrost/anarchybot/Scheduled/NewStatuses.java
+++ src/main/java/dev/elliotfrost/anarchybot/Scheduled/NewStatuses.java
@@ -4,9 +4,12 @@ import dev.elliotfrost.anarchybot.Bot;
 import dev.elliotfrost.anarchybot.Config;
 import net.dv8tion.jda.api.entities.Message;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class NewStatuses {
     public void run() {
@@ -30,5 +33,26 @@ public class NewStatuses {
         for (Message message : old) {
             Objects.requireNonNull(Objects.requireNonNull(Bot.getJDA().getGuildById(Config.get("GUILD-ID"))).getTextChannelById(Config.getStatus())).deleteMessageById(message.getId()).complete();
         }
+    }
+    public boolean checkMessage() {
+        String messageid = null;
+        try {
+            File myObj = new File("status-messages.txt");
+            Scanner myReader = new Scanner(myObj);
+            messageid = myReader.next();
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        Message message = null;
+        try {
+            message = Objects.requireNonNull(Objects.requireNonNull(Bot.getJDA().getGuildById(Config.get("GUILD-ID"))).getTextChannelById(Config.getStatus())).retrieveMessageById(Objects.requireNonNull(messageid)).complete();
+        } catch (Error e) {
+            System.out.println(e);
+        } finally {
+            if (message != null) return true;
+        }
+        return false;
     }
 }
